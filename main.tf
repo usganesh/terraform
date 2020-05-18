5078f5cb751d7db56c4d233f254aa316c2ea4e0c
SaravanaGanesh Ulgamani <u.s.ganesh@live.com>
2020-05-18 23:00:50 +0000

    tf sample create Ec2

--- main.tf
+++ main.tf
@@ -1,6 +1,5 @@
 provider "aws" {
-    access_key = "AKIAU2QQVWMQ63FYTUWI"
-    secret_key = "L7TRbcZ9Pfq4HnjJqy+uDlBiZaaH6sQN66eJ2rrd"
+    profile = "default"
     region     = "us-east-1"
 }
 resource "aws_instance" "example" {
