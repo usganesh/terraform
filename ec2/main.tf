b61c8603b18e97ec180b715c3252165195ee6cbc
SaravanaGanesh Ulagamani <u.s.ganesh@live.com>
2021-07-24 00:39:24 +0000

    ec2 tf files added

--- ec2/main.tf
+++ ec2/main.tf
@@ -3,6 +3,7 @@ provider "aws" {
     region     = "us-east-1"
 }
 resource "aws_instance" "example" {
-    ami           = "ami-0c278895328cddfdd"
+    ami           = "ami-03295ec1641924349"
     instance_type = "t2.micro"
+tags = {    Name = "ExampleAppServerInstance"  }
 }
